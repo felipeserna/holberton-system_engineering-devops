e66221412e1f9ba702aa1c3714198481e79c312b
Felipe Serna <feserna86@gmail.com>
2020-08-03 21:41:28 +0000

    2-execute_a_command.pp

---
+++ 0x0A-configuration_management/2-execute_a_command.pp
@@ -0,0 +1,6 @@
+# create a manifest that kills a process named killmenow.
+# * Must use the exec Puppet resource
+# * Must use pkill
+exec { 'pkill':
+  command    => '/usr/bin/pkill killmenow',
+  }
