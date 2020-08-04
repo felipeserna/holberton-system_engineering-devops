e3b0e14160b62ad777d6e0851bd1d930dcd4636f
Felipe Serna <feserna86@gmail.com>
2020-08-04 16:55:47 +0000

    4-puppet_ssh_config.pp

--- 0x0B-ssh/4-puppet_ssh_config.pp
+++ 0x0B-ssh/4-puppet_ssh_config.pp
@@ -1,15 +1,11 @@
 # set up your client SSH configuration file so that you can connect
 # to a server without typing a password.
-# * Your SSH client configuration must be configured to use the
-# private key ~/.ssh/holberton
-# * Your SSH client configuration must be configured to refuse
-# to authenticate using a password
-file_line { 'puppet_configuration':
+file_line { 'Declare identity file':
   path => '/etc/ssh/ssh_config',
   line => 'IdentityFile ~/.ssh/holberton',
 }
 
-file_line { 'puppet_configuration':
+file_line { 'Turn off passwd auth':
   path => '/etc/ssh/ssh_config',
   line => 'PasswordAuthentication no',
 }
