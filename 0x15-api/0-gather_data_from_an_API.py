9c4e297cba75e1216a9d9be3c4daf4e246ea9291
Felipe Serna <feserna86@gmail.com>
2020-09-14 22:41:47 +0000

    0-gather_data_from_an_API.py

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""Gather data from an API"""
+
+
+import requests
+import sys
+
+
+if __name__ == "__main__":
+    if len(sys.argv) >= 2 and sys.argv[1].isdigit():
+        api = "https://jsonplaceholder.typicode.com"
+        user_id = sys.argv[1]
+
+        data = requests.get("{}/users/{}".format(api, user_id)).json()
+        todos = requests.get("{}/users/{}/todos".format(api, user_id)).json()
+        done = 0
+        total = len(todos)
+        tasks = []
+
+        for task in todos:
+            if task.get("completed") is True:
+                done = done + 1
+                tasks.append("\t " + task.get("title"))
+
+        print("Employee {} is done with tasks({}/{}):".
+              format(data.get("name"), done, total))
+        print("\n".join(tasks))
