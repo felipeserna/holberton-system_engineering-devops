01e7275fdd5d5b09d41474af0a806a607d979902
Felipe Serna <feserna86@gmail.com>
2020-09-30 17:31:03 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,4 +1,4 @@
 # Fixes 500 error from wordpress config file
 exec {
-     '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php':
-  }
+    '/usr/bin/env sed -i "s/phpp/php/g" /var/www/html/wp-settings.php':
+}
