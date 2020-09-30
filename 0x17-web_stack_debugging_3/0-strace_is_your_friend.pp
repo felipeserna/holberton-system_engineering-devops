661836ac3e9ddcadb622eeab691843b84a0a946b
Felipe Serna <feserna86@gmail.com>
2020-09-30 18:45:12 +0000

    0-strace_is_your_friend.pp

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,5 +1,5 @@
 # Fixes 500 error from wordpress config file
-file { '/var/www/html/wp-includes/class-up-locale.phpp':
+file { '/var/www/html/wp-includes/class-wp-locale.phpp':
 ensure => present,
 source => '/var/www/html/wp-includes/class-wp-locale.php',
 }
